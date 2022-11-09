baee640b1b80a3bd35a24af68f01363a9348adfd
danielfcollier <danielfcollier@gmail.com>
2022-11-09 15:48:17 -0300

    Merge account definitions.

--- model/Account.cs
+++ model/Account.cs
@@ -6,9 +6,11 @@ namespace Model
 {
     public class Account
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [JsonPropertyName("id")]
         public string Id { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [JsonPropertyName("balance")]
         public decimal Balance { get; set; }
     }
--- model/Transaction.cs
+++ model/Transaction.cs
@@ -2,28 +2,16 @@
 
 using System.Text.Json.Serialization;
 
-namespace Transaction
+namespace Model
 {
-    public class Operation
+    public class Transaction
     {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [JsonPropertyName("origin")]
-        public Source? Origin { get; set; }
+        public Account? Origin { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [JsonPropertyName("destination")]
-        public Source? Destination { get; set; }
-    }
-
-    public class Source
-    {
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-        [JsonPropertyName("balance")]
-        public decimal Balance { get; set; }
+        public Account? Destination { get; set; }
     }
 }
