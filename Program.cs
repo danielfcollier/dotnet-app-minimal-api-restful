2d84aa634e4581fc1f2f7b8bc44b947aaa1e67de
danielfcollier <danielfcollier@gmail.com>
2022-11-09 10:56:08 -0300

    Add /reset true file handler.

--- Program.cs
+++ Program.cs
@@ -3,23 +3,24 @@ using Model;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapPost("/reset", () =>
+app.MapPost("/reset", async () =>
 {
-  return Results.Ok("OK");
+    await Db.Handler.Reset();
+    return Results.Ok("OK");
 });
 
 app.MapGet("/balance", async (HttpRequest request) =>
 {
-  string id = request.Query["account_id"];
-  
-  Account? data = await Db.Handler.Read(id);
+string id = request.Query["account_id"];
 
-  if (data is not null)
-  {
+Account? data = await Db.Handler.Read(id);
+
+if (data is not null)
+{
     return Results.Ok(data.Balance);
-  }
+}
 
-  return Results.NotFound(0);
+return Results.NotFound(0);
 });
 
 app.Run("http://localhost:4000");
