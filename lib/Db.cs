6b03e5dfc6c08594640db75f611482c980f5153a
danielfcollier <danielfcollier@gmail.com>
2022-11-09 13:35:59 -0300

    Update Db Handlers and add functionalities.

---
+++ lib/Db.cs
@@ -0,0 +1,62 @@
+using System.Text.Json;
+
+using Model;
+
+namespace Db
+{
+    public static class Handler
+    {
+        private readonly static string DB_FILE = "data.json";
+        private readonly static string filepath = Path.Join(".", "db", DB_FILE);
+
+        private static async Task<List<Account>?> ReadAll()
+        {
+            var content = await File.ReadAllTextAsync(filepath);
+
+            if (content is null || content == String.Empty)
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<List<Account>>(content);
+        }
+
+        public static async Task Reset()
+        {
+            await File.WriteAllTextAsync(filepath, String.Empty);
+        }
+
+        public static async Task Create(Account account)
+        {
+            var data = await ReadAll();
+
+            if (data is null)
+            {
+                return;
+            }
+
+            data.Add(account);
+            await File.WriteAllTextAsync(filepath, JsonSerializer.Serialize(data));
+        }
+
+        public static async Task<Account?> Read(string id)
+        {
+            var data = await ReadAll();
+
+            if (data is null)
+            {
+                return null;
+            }
+
+            foreach (var element in data)
+            {
+                if (element.Id == id)
+                {
+                    return element;
+                }
+            }
+
+            return null;
+        }
+    }
+}
