489c95dee73605462915f9bb7a7fe4bdda317b2a
danielfcollier <danielfcollier@gmail.com>
2022-11-09 10:55:19 -0300

    nit: adjust identation.

--- model/Account.cs
+++ model/Account.cs
@@ -4,12 +4,12 @@ using System.Text.Json.Serialization;
 
 namespace Model
 {
-  public class Account
-  {
-    [JsonPropertyName("id")]
-    public string Id { get; set; }
+    public class Account
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
 
-    [JsonPropertyName("balance")]
-    public decimal Balance { get; set; }
-  }
+        [JsonPropertyName("balance")]
+        public decimal Balance { get; set; }
+    }
 }
